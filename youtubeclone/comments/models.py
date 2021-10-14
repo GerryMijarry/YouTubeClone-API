1ab5fc34c54581103733a5f0d5bb6b4a7c32ff49
ForbesR86 <89602329+ForbesR86@users.noreply.github.com>
2021-10-14 10:42:04 -0400

    models update
    models update, serializer update as per JJ

--- youtubeclone/comments/models.py
+++ youtubeclone/comments/models.py
@@ -5,16 +5,11 @@ from django.db import models
 class Comment(models.Model):
     videoid = models.TextField(max_length=300)
     comment = models.TextField(max_length=300)
-    datetime = models.DateTimeField(auto_now=True, blank=False, null=False)
-    name = models.TextField(max_length=300)
     likes = models.IntegerField(blank=False, null=False)
     dislikes = models.IntegerField(blank=False, null=False)
 
 class Reply(models.Model):
     commentid = models.ForeignKey(Comment, on_delete=models.CASCADE)
     reply = models.TextField(max_length=300)
-    datetime = models.DateTimeField(auto_now=True, blank=False, null=False)
     name = models.TextField(max_length=300)
-    likes = models.IntegerField(blank=False, null=False)
-    dislikes = models.IntegerField(blank=False, null=False)
     
